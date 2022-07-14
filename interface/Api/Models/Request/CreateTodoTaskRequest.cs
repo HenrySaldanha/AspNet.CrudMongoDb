00a9fc5763890a83d667b029b87edcbc3be44ccb
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-13 21:01:24 -0300

    Add request and response

---
+++ interface/Api/Models/Request/CreateTodoTaskRequest.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+
+namespace Api.Models.Request;
+public class CreateOrUpdateTodoTaskRequest
+{
+    public string Description { get; set; }
+    public IEnumerable<CreateOrUpdateTodoTaskRequest>? Chields { get; set; }
+
+    public static implicit operator TodoTask(CreateOrUpdateTodoTaskRequest request)
+    {
+        if (request is null)
+            return null;
+
+        return new TodoTask
+        {
+            Id = Guid.NewGuid(),
+            CreationDate = DateTime.UtcNow,
+            FinishDate = null,
+            IsDone = false,
+            Description = request.Description,
+            Chields = request.Chields?.Select(c => (TodoTask)c)
+        };
+    }
+}
---
+++ interface/Api/Models/Response/TodoTaskResponse.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+
+namespace Api.Models.Response;
+public class TodoTaskResponse
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+    public bool IsDone { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime? FinishDate { get; set; }
+    public IEnumerable<TodoTaskResponse>? Chields { get; set; }
+
+    public static implicit operator TodoTaskResponse(TodoTask task)
+    {
+        if (task is null)
+            return null;
+
+        return new TodoTaskResponse
+        {
+            Id = task.Id,
+            Description = task.Description,
+            IsDone = task.IsDone,
+            CreationDate = task.CreationDate,
+            FinishDate = task.FinishDate,
+            Chields = task.Chields?.Select(c => (TodoTaskResponse)c)
+        };
+    }
+}
